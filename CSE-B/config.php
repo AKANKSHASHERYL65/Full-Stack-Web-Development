d994e2b1b3c94786d5fe7b52547773c39dd4d7fb
AKANKSHASHERYL65 <akankshasherylm@gmail.com>
2020-01-28 09:15:15 +0530

    msg

--- CSE-B/config.php
+++ CSE-B/config.php
@@ -2,7 +2,7 @@
 $hostName = 'localhost';
 $userName = 'root';
 $password = '';
-$databaseName = 'CSEB2020';
+$databaseName = 'cseb2020';
     
 $conn = mysqli_connect($hostName, $userName, $password, $databaseName);
 if ($conn){
